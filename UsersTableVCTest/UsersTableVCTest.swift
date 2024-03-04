00395eb05d53a14a77d36cc6282d4dd327a38aa7
abdul ahad <abdul_ahad1996@live.com>
2024-03-04 03:46:01 +0100

    can init Vc

--- UsersTableVCTest/UsersTableVCTest.swift
+++ UsersTableVCTest/UsersTableVCTest.swift
@@ -6,7 +6,18 @@
 //
 
 import XCTest
+@testable import EDeveloperMVC
 
 final class UsersTableVCTest: XCTestCase {
 
+    func testCanInit() throws {
+        let bundle = Bundle(for: UsersTableVC.self)
+        let sb = UIStoryboard(name: "Main", bundle: bundle)
+        
+        let initialVC = sb.instantiateInitialViewController()
+        let navigation = try XCTUnwrap(initialVC as? UINavigationController)
+        
+       let _ = try XCTUnwrap(navigation.topViewController as? UsersTableVC)
+        
+    }
 }
